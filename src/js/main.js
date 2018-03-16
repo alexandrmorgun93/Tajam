9c6078748d74d1c0b58cacd9725a334300e1e0f1
alexandrmorgun93 <alexandr.morgun93@gmail.com>
2018-03-17 01:17:07 +0200

    page

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,47 @@
 ;
 (() => {
-  'use strict';
-  
-  /*variables*/
+    'use strict';
+
+    /*variables*/
 
 
+    const Parallax = require('scroll-parallax');
+    let p = new Parallax('.parallax', {
+        intensity: 50
+    }).init();
+
+    // video
+    let video = document.querySelector('video');
+    let playBtn = document.querySelector('.play_video');
+    let stopBtn = document.querySelector('.stop_video');
+    let videoFade = document.querySelectorAll('.video_fade');
+
+
+    playBtn.addEventListener('click', () => {
+        play();
+        fadeGo();
+        playBtn.classList.add('active');
+    });
+    stopBtn.addEventListener('click', () => {
+        pause();
+        playBtn.classList.remove('active');
+        for (let i = 0; i < videoFade.length; i++) {
+            videoFade.classList.remove('active');
+        }
+    });
+
+    function fadeGo() {
+        for (let i = 0; i < videoFade.length; i++) {
+            videoFade.classList.add('active');
+        }
+    }
+
+    function play() {
+        video.play();
+    }
+
+    function pause() {
+        video.pause();
+    }
 
 })();
